dfc27e0a68fd281c9bbd09a8e69453e58cb6c214
Foxfare <foxfare@users.noreply.github.com>
2022-06-13 23:05:23 +0200

    main-page-v2

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const header = document.querySelector("header");
 const sectionOne = document.querySelector(".land-container");
+const logo = document.querySelector(".logo-text");
 
 const sectionOneOptions = {
-    rootMargin: "10px 0px 0px 0px"
+    rootMargin: "-100px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
     entries.forEach(entry => {
         if(!entry.isIntersecting){
             header.classList.add('scrolled');
+            logo.classList.add('logo-text-hidden');
         }
         else{
             header.classList.remove('scrolled');
+            logo.classList.remove('logo-text-hidden');
         }
     })
 }, sectionOneOptions);
