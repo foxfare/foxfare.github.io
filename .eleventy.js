15529d7061fab3e7bcf05242072fc284595514e2
Foxfare <foxfare@users.noreply.github.com>
2022-06-15 15:33:00 +0200

    11ty update

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const {DateTime} = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/main.css');
+    eleventyConfig.addPassthroughCopy('./src/main.js');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
