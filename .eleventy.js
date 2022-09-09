4de8af0ed431820b037ac0c78f0d3ea73d49a034
foxfare <107399419+foxfare@users.noreply.github.com>
2022-09-09 17:38:09 +0200

    .

--- .eleventy.js
+++ .eleventy.js
@@ -18,3 +18,19 @@ module.exports = function(eleventyConfig) {
         }
     };
 }
+
+function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList)
+
+  // Create an array of all tags
+  eleventyConfig.addCollection("tagList", function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(item => {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    });
+
+    return filterTagList([...tagSet]);
+  });
